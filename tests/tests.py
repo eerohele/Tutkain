e06eeff3fe90ab209db14b9da34ae9c53ac9478a
Eero Helenius <eero.helenius@pm.me>
2020-03-26 21:41:58 +0200

    Fix capitalization

--- tests/tests.py
+++ tests/tests.py
@@ -10,12 +10,12 @@ sys.path.append(os.path.join(os.path.dirname(__file__), 'dependencies'))
 import edn_format
 from edn_format import Keyword
 
-Tutkain = sys.modules["Tutkain.tutkain"]
+tutkain = sys.modules["Tutkain.tutkain"]
 
 
 class TestReplClient(TestCase):
     def test_repl_client(self):
         # You have to start a socket REPL on localhost:12345 to run this test.
-        with Tutkain.ReplClient('localhost', 12345) as repl_client:
+        with tutkain.ReplClient('localhost', 12345) as repl_client:
             repl_client.input.put('(+ 1 2 3)')
             self.assertEquals(repl_client.output.get().get(Keyword('val')), '6')
