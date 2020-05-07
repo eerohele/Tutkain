a8ab501dd97351c063960e8486c73415db04fafd
Eero Helenius <eero.helenius@pm.me>
2020-05-07 22:33:50 +0300

    Disable pretty-printing
    Well, clojure.pprint/write obviously writes things into STDOUT, which
    means nREPL returns everything under :out and nothing under :value.

--- op.py
+++ op.py
@@ -5,7 +5,6 @@ def eval(opts):
     return {
         'op': 'eval',
         'id': str(uuid.uuid4()),
-        'nrepl.middleware.print/print': 'clojure.pprint/write',
         'nrepl.middleware.caught/print?': 'true',
         'session': opts.get('session'),
         'code': opts.get('code')
