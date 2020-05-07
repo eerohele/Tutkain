74abd83a6d9ee9a106e5e398e69a027c6519b7e9
Eero Helenius <eero.helenius@pm.me>
2020-05-07 14:09:39 +0300

    Print exception stack traces

--- op.py
+++ op.py
@@ -6,6 +6,7 @@ def eval(opts):
         'op': 'eval',
         'id': str(uuid.uuid4()),
         'nrepl.middleware.print/print': 'clojure.pprint/write',
+        'nrepl.middleware.caught/print?': 'true',
         'session': opts.get('session'),
         'code': opts.get('code')
     }
