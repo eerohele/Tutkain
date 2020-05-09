71f15a7c3c197dc1b3b6504f296103d0ac274c5f
Eero Helenius <eero.helenius@pm.me>
2020-05-09 21:17:48 +0300

    Optimize tutkain.brackets tests
    No need to recreate the view after every test — clearing it should be
    enough.

--- tests/test_brackets.py
+++ tests/test_brackets.py
@@ -5,18 +5,24 @@ import tutkain.brackets as brackets
 
 
 class TestBrackets(TestCase):
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         self.view = sublime.active_window().new_file()
         self.view.set_scratch(True)
         self.view.sel().clear()
         self.view.window().focus_view(self.view)
         self.view.set_syntax_file('Packages/Clojure/Clojure.tmLanguage')
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         if self.view:
             self.view.window().run_command('close_file')
 
-    def set_view_content(self, chars):
+    def setUp(self):
+        self.view.run_command('select_all')
+        self.view.run_command('right_delete')
+
+    def append_to_view(self, chars):
         self.view.run_command('append', {'characters': chars})
 
     def form(self, pos):
@@ -24,33 +30,33 @@ class TestBrackets(TestCase):
 
     def test_current_form_region_simple(self):
         form = '(+ 1 2)'
-        self.set_view_content(form)
+        self.append_to_view(form)
         for n in range(len(form)):
             self.assertEquals(self.form(n), form)
 
     def test_current_form_region_brackets(self):
         form = '[1 [2 3] 4]'
-        self.set_view_content(form)
+        self.append_to_view(form)
         self.assertEquals(self.form(0), form)
         self.assertEquals(self.form(len(form)), form)
         self.assertEquals(self.form(3), '[2 3]')
 
     def test_current_form_region_mixed(self):
         form = '(a {:b :c} [:d] )'
-        self.set_view_content(form)
+        self.append_to_view(form)
         self.assertEquals(self.form(5), '{:b :c}')
         self.assertEquals(self.form(11), '[:d]')
         self.assertEquals(self.form(16), form)
 
     def test_current_form_region_set(self):
         form = '#{1 2 3}'
-        self.set_view_content(form)
+        self.append_to_view(form)
         for n in range(len(form)):
             self.assertEquals(self.form(n), form)
 
     def test_current_form_region_discard(self):
         form = '(inc #_(dec 2) 4)'
-        self.set_view_content(form)
+        self.append_to_view(form)
         self.assertEquals(self.form(7), '(dec 2)')
         self.assertEquals(self.form(12), '(dec 2)')
         self.assertEquals(self.form(12), '(dec 2)')
@@ -59,15 +65,15 @@ class TestBrackets(TestCase):
 
     def test_string_next_to_lbracket(self):
         form = '(merge {"A" :B})'
-        self.set_view_content(form)
+        self.append_to_view(form)
         self.assertEquals(self.form(len(form)), form)
 
     def test_ignore_string_1(self):
         form = '(a "()" b)'
-        self.set_view_content(form)
+        self.append_to_view(form)
         self.assertEquals(self.form(len(form)), form)
 
     def test_ignore_string_2(self):
         form = '(a "\"()\"" b)'
-        self.set_view_content(form)
+        self.append_to_view(form)
         self.assertEquals(self.form(len(form)), form)
