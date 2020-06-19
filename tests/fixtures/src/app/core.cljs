727aec77646a3145d83d6489810ba0b5ee8d2a1b
Eero Helenius <eero.helenius@pm.me>
2020-06-19 22:01:44 +0300

    Set up ClojureScript REPL via Piggieback

--- tests/fixtures/src/app/core.cljs
+++ tests/fixtures/src/app/core.cljs
@@ -5,5 +5,10 @@
   (js/parseInt x 10))
 
 (comment
+  ; Evaluate these before evaluating the view.
+  (require 'cider.piggieback)
+  (require '[cljs.repl.node])
+  (cider.piggieback/cljs-repl (cljs.repl.node/repl-env))
+
   (parse-int "42")
   )
