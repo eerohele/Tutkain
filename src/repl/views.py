fbc276649f41f6ebc1daa80ad517fe8311dcb85d
Eero Helenius <eero.helenius@solita.fi>
2021-02-01 12:17:17 +0200

    Make stack traces somewhat clickable
    If you click on a stack trace line that points to a file that's inside
    the current project folder, Sublime Text will try to open the file at
    that line and column. Works OK when there aren't multiple files with the
    same name.

--- src/repl/views.py
+++ src/repl/views.py
@@ -24,6 +24,7 @@ def create(window, host, port):
     view.settings().set("tutkain_repl_output_view", True)
     view.settings().set("tutkain_repl_host", host)
     view.settings().set("tutkain_repl_port", port)
+    view.settings().set("result_file_regex", r"""\s*\[.+?\"(.+?)" (\d+)\]""")
     view.set_read_only(True)
     view.set_scratch(True)
 
