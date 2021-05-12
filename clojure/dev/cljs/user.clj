f8159e85e624cc6fc015581b67a26ac9f0cee772
Eero Helenius <eero.helenius@solita.fi>
2021-05-12 13:45:54 +0300

    Update ClojureScript REPL configuration
    Still doesn't work for shit, but hopefully a step in the right
    direction.

--- clojure/dev/cljs/user.clj
+++ clojure/dev/cljs/user.clj
@@ -1,21 +1,26 @@
 (ns user
   (:require
+   [cljs.analyzer.api :as analyzer.api]
    [cljs.build.api :as build]
    [cljs.env :as env]
    [cljs.repl :as repl]
    [cljs.repl.browser :as browser]))
 
 (comment
-  (build/build ["dev/src"]
+  (build/build "dev/src"
     {:main 'my.app
-     :output-to "out/app.js"
-     :output-dir "out"
-     :target :browser}
-    tutkain.repl.runtime.cljs/*compiler-env*)
+     :output-to "out/main.js"
+     :target :browser
+     :verbose false})
 
   (repl/repl (browser/repl-env)
+    :init #(println :done)
+    :watch "dev/src"
+    :output-dir "out"
     :need-prompt (constantly false)
-    :prompt (constantly ""))
+    :prompt (constantly "")
+    :print tutkain.repl/*print*
+    :caught tutkain.repl/*caught*)
 
   :cljs/quit
   )
---
+++ clojure/dev/src/user.cljs
@@ -0,0 +1,7 @@
+(ns cljs.user
+  (:require [clojure.browser.repl :as repl]))
+
+(defonce conn
+  (repl/connect "http://localhost:9000/repl"))
+
+(enable-console-print!)
