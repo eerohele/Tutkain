260e6058b063fb05805f214d1e90d81de1adbe86
Eero Helenius <eero.helenius@solita.fi>
2021-05-15 22:16:22 +0300

    Set :infer-externs to true

--- clojure/dev/src/my/node/app.cljs
+++ clojure/dev/src/my/node/app.cljs
@@ -10,10 +10,16 @@
   (println "Hello, world!"))
 
 (defn read-file
-  [^js path]
+  [path]
   (.readFileSync fs path #js {:encoding "utf-8"}))
 
 (comment
   (let [lines (string/split (read-file "/etc/hosts") #"\n")]
-    (remove #(string/starts-with? % "#") lines))
+    (->>
+      lines
+      (sequence
+        (comp
+          (remove #(string/starts-with? % "#"))
+          (map #(string/split % #"\s+"))))
+      (sort)))
   ,)
