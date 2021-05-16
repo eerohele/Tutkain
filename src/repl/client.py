0418df491e5a2a91639fb028e226c67e91e00732
Eero Helenius <eero.helenius@solita.fi>
2021-05-16 15:33:44 +0300

    Fix another Windows path issue

--- src/repl/client.py
+++ src/repl/client.py
@@ -215,7 +215,7 @@ class JVMClient(Client):
         with open(path, "rb") as file:
             blob = base64.b64encode(file.read()).decode("utf-8")
             backchannel_port = self.backchannel_opts.get("port", 0)
-            self.write_line(f"""#?(:bb (do (prn {{:tag :ret :val "{{}}"}}) ((requiring-resolve 'clojure.core.server/io-prepl))) :clj (do (with-open [reader (-> (java.util.Base64/getDecoder) (.decode "{blob}") (java.io.ByteArrayInputStream.) (java.io.InputStreamReader.) (clojure.lang.LineNumberingPushbackReader.))] (clojure.lang.Compiler/load reader "{os.path.abspath(path)}" "{os.path.basename(path)}")) (try (tutkain.repl/repl {{:port {backchannel_port}}}) (catch Exception ex {{:tag :err :val (.toString ex)}}))))""")
+            self.write_line(f"""#?(:bb (do (prn {{:tag :ret :val "{{}}"}}) ((requiring-resolve 'clojure.core.server/io-prepl))) :clj (do (with-open [reader (-> (java.util.Base64/getDecoder) (.decode "{blob}") (java.io.ByteArrayInputStream.) (java.io.InputStreamReader.) (clojure.lang.LineNumberingPushbackReader.))] (clojure.lang.Compiler/load reader "{path}" "{os.path.basename(path)}")) (try (tutkain.repl/repl {{:port {backchannel_port}}}) (catch Exception ex {{:tag :err :val (.toString ex)}}))))""")
 
         line = self.buffer.readline()
 
