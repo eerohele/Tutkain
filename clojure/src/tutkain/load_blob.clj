f921e45a66b89b0fe11f1f1003b10df0e464167f
Eero Helenius <eero.helenius@solita.fi>
2021-05-22 17:57:15 +0300

    Pretty-print :load op result

--- clojure/src/tutkain/load_blob.clj
+++ clojure/src/tutkain/load_blob.clj
@@ -6,20 +6,15 @@
    (clojure.lang Compiler LineNumberingPushbackReader)
    (java.io File StringReader)))
 
-(defmulti load-blob :dialect)
-
-(defmethod load-blob :default
+(defmethod handle :load
   [{:keys [code file] :as message}]
   (try
     (with-open [reader (LineNumberingPushbackReader. (StringReader. code))]
       (let [file-name (some-> file File. .getName)
-            val (Compiler/load reader file file-name)]
-        (respond-to message {:tag :ret :val (pr-str val)})))
+            val (Compiler/load reader (or file "NO_SOURCE_FILE") file-name)]
+        (respond-to message {:tag :ret
+                             :val (pp-str val)})))
     (catch Throwable ex
       (respond-to message {:tag :ret
                            :val (pp-str (assoc (Throwable->map ex) :phase :execution))
                            :exception true}))))
-
-(defmethod handle :load
-  [message]
-  (load-blob message))
