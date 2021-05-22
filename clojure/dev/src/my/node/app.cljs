9eb4d0f4f0acded037f50748c4f4af09dfc558bc
Eero Helenius <eero.helenius@solita.fi>
2021-05-22 17:24:07 +0300

    Rewrite test expression

--- clojure/dev/src/my/node/app.cljs
+++ clojure/dev/src/my/node/app.cljs
@@ -14,9 +14,12 @@
   (.readFileSync fs path #js {:encoding "utf-8"}))
 
 (comment
-  (into (sorted-set)
-    (comp
-      (remove #(string/starts-with? % "#"))
-      (map #(string/split % #"\s+")))
-    (string/split (read-file "/etc/hosts") #"\n"))
+  (def remove-comments (remove #(string/starts-with? % "#")))
+  (def split-by-whitespace (map #(string/split % #"\s+")))
+
+  (->>
+    (read-file "/etc/hosts")
+    (string/split-lines)
+    (into (sorted-set)
+      (comp remove-comments split-by-whitespace)))
   ,)
