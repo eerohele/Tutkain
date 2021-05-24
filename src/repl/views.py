0e4fba0dbe06310444f543bc6f376fed326158b2
Eero Helenius <eero.helenius@solita.fi>
2021-05-24 08:30:52 +0300

    Set REPL view scroll_past_end to 0.5
    Ought to let users override at least some of the REPL view settings.

--- src/repl/views.py
+++ src/repl/views.py
@@ -37,6 +37,7 @@ def create(window, dialect, client):
     view.settings().set("tutkain_repl_view_dialect", dialect.name or "clj")
     view.settings().set("tutkain_repl_host", client.host)
     view.settings().set("tutkain_repl_port", client.port)
+    view.settings().set("scroll_past_end", 0.5)
     view.settings().set("result_file_regex", r"""\s*\[.+?\"(.+?)" (\d+)\]""")
     view.set_read_only(True)
     view.set_scratch(True)
