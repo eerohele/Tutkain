a5c839b252c3f1824313c28494986d973f6eff42
Eero Helenius <eero.helenius@solita.fi>
2021-05-27 20:30:46 +0300

    Add tests for dialects.for_point

--- tests/test_dialect.py
+++ tests/test_dialect.py
@@ -4,7 +4,7 @@ from Tutkain.src import dialects
 from .util import ViewTestCase
 
 
-class TestDialect(ViewTestCase):
+class TestViewDialect(ViewTestCase):
     def test_get_view_dialect(self):
         self.view.assign_syntax("Packages/Tutkain/Clojure (Tutkain).sublime-syntax")
         self.assertEquals(edn.Keyword("clj"), dialects.for_view(self.view))
@@ -16,3 +16,19 @@ class TestDialect(ViewTestCase):
         self.assertEquals(edn.Keyword("cljs"), dialects.for_view(self.view))
         self.view.window().settings().set("tutkain_evaluation_dialect", "clj")
         self.assertEquals(edn.Keyword("clj"), dialects.for_view(self.view))
+
+
+class TestPointDialect(ViewTestCase):
+    def test_get_point_dialect(self):
+        self.view.assign_syntax("Packages/Tutkain/Clojure (Tutkain).sublime-syntax")
+        self.set_view_content("""(Integer/parseInt "42")""")
+        self.assertEqual(edn.Keyword("clj"), dialects.for_point(self.view, 0))
+        self.view.assign_syntax("Packages/Tutkain/ClojureScript (Tutkain).sublime-syntax")
+        self.set_view_content("""(js/parseInt "42")""")
+        self.assertEqual(edn.Keyword("cljs"), dialects.for_point(self.view, 0))
+        self.view.assign_syntax("Packages/Tutkain/Clojure Common (Tutkain).sublime-syntax")
+        self.set_view_content("""(inc 1)""")
+        self.assertEqual(edn.Keyword("clj"), dialects.for_point(self.view, 0))
+        self.view.assign_syntax("Packages/Markdown/Markdown.sublime-syntax")
+        self.set_view_content("""# Hello, world!""")
+        self.assertEqual(None, dialects.for_point(self.view, 0))
