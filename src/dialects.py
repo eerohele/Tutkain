5c22e98077f4c9cce8cf7d4ac221a2758f6a37c5
Eero Helenius <eero.helenius@solita.fi>
2021-05-27 20:59:32 +0300

    Add dialects.evaluation_dialect helper

--- src/dialects.py
+++ src/dialects.py
@@ -1,6 +1,8 @@
+from sublime import View
 from ..api import edn
 from . import state
 from .repl import views
+from typing import Union
 
 
 DIALECT_NAMES = edn.kwmap({
@@ -15,28 +17,30 @@ def name(dialect: edn.Keyword) -> str:
     return DIALECT_NAMES.get(dialect, "")
 
 
-def for_point(view, point):
-    if view.match_selector(point, "source.clojure.clojure-common") and (
-        eval_dialect := view.window().settings().get(
-            "tutkain_evaluation_dialect"
-        )
+def evaluation_dialect(view: Union[View, None]) -> Union[edn.Keyword, None]:
+    """Given a Tutkain REPL view, return the evaluation dialect for the
+    window the view belongs to."""
+    if view and (window := view.window()) and (
+        dialect := window.settings().get("tutkain_evaluation_dialect")
     ):
-        return edn.Keyword(eval_dialect)
+        return edn.Keyword(dialect)
+
+
+def for_point(view: View, point: int) -> Union[edn.Keyword, None]:
+    if view.match_selector(point, "source.clojure.clojure-common"):
+        return evaluation_dialect(view) or edn.Keyword("clj")
     if view.match_selector(point, "source.clojure.clojurescript"):
         return edn.Keyword("cljs")
     if view.match_selector(point, "source.clojure.babashka"):
         return edn.Keyword("bb")
-
-    return edn.Keyword("clj")
+    if view.match_selector(point, "source.clojure"):
+        return edn.Keyword("clj")
 
 
 def for_view(view):
     if syntax := view.syntax():
         if syntax.scope == "source.clojure.clojure-common":
-            return edn.Keyword(view.window().settings().get(
-                "tutkain_evaluation_dialect",
-                "clj"
-            ))
+            return evaluation_dialect(view) or edn.Keyword("clj")
         if syntax.scope == "source.clojure.clojurescript":
             return edn.Keyword("cljs")
         if syntax.scope == "source.clojure.babashka":
