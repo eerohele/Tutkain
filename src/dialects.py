5b13e06fb12a418bf1a4f01ee90c75dc0b022863
Eero Helenius <eero.helenius@solita.fi>
2021-05-27 20:46:16 +0300

    Refactor focus_view to use views.get_dialect

--- src/dialects.py
+++ src/dialects.py
@@ -45,18 +45,13 @@ def for_view(view):
             return edn.Keyword("clj")
 
 
-def focus_view(view, dialect):
+def focus_view(view: View, dialect: edn.Keyword) -> None:
     """Given an initial view and a dialect, if the currently active REPL view
     has a different dialect than the given dialect, focus the active REPL view
     for the given dialect, then refocus the initial view."""
-    window = view.window()
-    active_repl_view = views.active_repl_view(window)
-
-    active_dialect = edn.Keyword(
-        active_repl_view.settings().get("tutkain_repl_view_dialect")
-    )
-
-    if active_dialect != dialect:
+    if (window := view.window()) and (
+        active_repl_view := views.active_repl_view(window)
+    ) and views.get_dialect(active_repl_view) != dialect:
         # Focus the REPL view for the given dialect
         window.focus_view(state.repl_view(window, dialect))
         # Focus the view that was initially active
