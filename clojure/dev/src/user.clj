fe9727ea411cc963b4358fc69b4b450960441133
Eero Helenius <eero.helenius@solita.fi>
2021-06-11 22:14:59 +0300

    Add benchmark utilities

--- clojure/dev/src/user.clj
+++ clojure/dev/src/user.clj
@@ -1,5 +1,7 @@
 (ns user
-  (:require [cognitect.transcriptor :as xr]))
+  (:require
+   [cognitect.transcriptor :as xr]
+   [criterium.core :refer [quick-bench]]))
 
 (defn run-tests
   []
@@ -12,4 +14,25 @@
 
 (comment
   (run-tests)
+  )
+
+(comment
+  (require
+    '[clojure.repl :as repl]
+    '[tutkain.analyzer :refer [local-symbol-positions]]
+    '[tutkain.completions :refer [candidates]])
+
+  (quick-bench (candidates "java." 'clojure.core))
+
+  (quick-bench
+    (local-symbol-positions
+      {:file "clojure/core.clj"
+       :ns (the-ns 'clojure.core)
+       :context (with-out-str (repl/source clojure.core/doseq))
+       :form 'seq-exprs
+       :start-line 0
+       :start-column 0
+       :line 5
+       :column 4
+       :end-column 13}))
   )
