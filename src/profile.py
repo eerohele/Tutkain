d31af19060771d0eec8b972d1d872394417b4861
Eero Helenius <eero.helenius@solita.fi>
2021-06-13 14:23:25 +0300

    Add profiling module

---
+++ src/profile.py
@@ -0,0 +1,17 @@
+import cProfile
+import functools
+
+
+def profile(func):
+    @functools.wraps(func)
+    def inner(*args, **kwargs):
+        profiler = cProfile.Profile()
+        profiler.enable()
+        try:
+            retval = func(*args, **kwargs)
+        finally:
+            profiler.disable()
+            profiler.dump_stats('/tmp/tutkain.prof')
+        return retval
+
+    return inner
