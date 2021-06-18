50e9273f40da938863ccf5ac9eab1e9fca525c5b
Eero Helenius <eero.helenius@solita.fi>
2021-06-18 15:25:12 +0300

    Rename tutkain_select_local_symbols command

--- package.py
+++ package.py
@@ -1215,7 +1215,7 @@ def positions_to_tuples(view, positions):
     return regions
 
 
-class TutkainSelectLocalSymbolsCommand(TextCommand):
+class TutkainSelectUsagesCommand(TextCommand):
     def handler(self, regions):
         # TODO: Why?
         self.view.run_command("tutkain_add_regions", {
