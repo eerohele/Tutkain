12dbaaad6c0b5b4ad1badb4461f4067f448ebd9e
Eero Helenius <eero.helenius@pm.me>
2021-09-06 09:20:26 +0300

    Refactor :apropos and :dir handlers

--- clojure/src/tutkain/query.clj
+++ clojure/src/tutkain/query.clj
@@ -10,39 +10,36 @@
 (defmethod handle :apropos
   [{:keys [pattern] :as message}]
   (when-some [re (some-> pattern not-empty re-pattern)]
-    (let [metas (sort-by :name
-                  (sequence
-                    (comp
-                      (map ns-interns)
-                      (mapcat vals)
-                      (map meta)
-                      (filter :doc)
-                      (filter (fn [{:keys [doc name]}]
-                                (or
-                                  (re-find (re-matcher re doc))
-                                  (re-find (re-matcher re (str name))))))
-                      (map lookup/prep-meta)
-                      (map add-type))
-                    (all-ns)))]
-      (respond-to message {:vars metas}))))
+    (let [vars (eduction
+                  (map ns-interns)
+                  (mapcat vals)
+                  (map meta)
+                  (filter :doc)
+                  (filter (fn [{:keys [doc name]}]
+                            (or
+                              (re-find (re-matcher re doc))
+                              (re-find (re-matcher re (str name))))))
+                  (map lookup/prep-meta)
+                  (map add-type)
+                  (all-ns))]
+      (respond-to message {:vars (sort-by :name vars)}))))
 
 (defmethod handle :dir
   [{:keys [ns sym] :as message}]
   (let [sym (symbol sym)
         ns (or (some-> ns symbol find-ns) (the-ns 'user))]
     (when-some [sym-ns (or
-                          ;; symbol naming ns
+                         ;; symbol naming ns
                          (some-> sym symbol find-ns)
                          ;; ns alias symbol
                          (get (ns-aliases ns) sym)
                          ;; non-ns symbol
                          (some-> (ns-resolve ns sym) .ns))]
-      (when-some [vars (sort-by :name
-                         (eduction
-                           (map val)
-                           (map meta)
-                           (map lookup/prep-meta)
-                           (map add-type)
-                           (ns-publics sym-ns)))]
+      (let [vars (eduction
+                   (map val)
+                   (map meta)
+                   (map lookup/prep-meta)
+                   (map add-type)
+                   (ns-publics sym-ns))]
         (respond-to message {:symbol (name sym)
-                             :vars vars})))))
+                             :vars (sort-by :name vars)})))))
