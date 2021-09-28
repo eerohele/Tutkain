4261c362b28115491e87f9517a3cdbb739f22f6f
Pedro Girardi <pedro@plando.work>
2021-09-28 18:33:57 +0700

    Explicit output views to be cleared

--- tests/test_clear.py
+++ tests/test_clear.py
@@ -10,6 +10,12 @@ def view_content(view):
     return view.substr(sublime.Region(0, view.size()))
 
 
+def reset_view(view):
+    view.set_read_only(False)
+    view.run_command("select_all")
+    view.run_command("right_delete")
+
+
 class TestClear(TestCase):
     def setUp(self):
         self.window = sublime.active_window()
@@ -25,8 +31,69 @@ class TestClear(TestCase):
 
     def test_clear(self):
         views.configure(self.repl_view, edn.Keyword("clj"), "localhost", 1234)
+
+        # Clear both views by default:
+
         self.repl_view.run_command("append", {"characters": "foo"})
         self.tap_panel.run_command("append", {"characters": "bar"})
+
         self.window.run_command("tutkain_clear_output_view")
+
         self.assertEquals("", view_content(self.repl_view))
         self.assertEquals("", view_content(self.tap_panel))
+
+
+        # Explicit pass "tap" and "repl":
+
+        reset_view(self.repl_view)
+        reset_view(self.tap_panel)
+
+        self.repl_view.run_command("append", {"characters": "foo"})
+        self.tap_panel.run_command("append", {"characters": "bar"})
+
+        self.window.run_command("tutkain_clear_output_view", {"output_views": ["tap", "repl"]})
+
+        self.assertEquals("", view_content(self.repl_view))
+        self.assertEquals("", view_content(self.tap_panel))
+
+
+        # Clear "tap" only:
+
+        reset_view(self.repl_view)
+        reset_view(self.tap_panel)
+
+        self.repl_view.run_command("append", {"characters": "foo"})
+        self.tap_panel.run_command("append", {"characters": "bar"})
+
+        self.window.run_command("tutkain_clear_output_view", {"output_views": ["tap"]})
+
+        self.assertEquals("foo", view_content(self.repl_view))
+        self.assertEquals("", view_content(self.tap_panel))
+
+
+        # Clear "repl" only:
+
+        reset_view(self.repl_view)
+        reset_view(self.tap_panel)
+
+        self.repl_view.run_command("append", {"characters": "foo"})
+        self.tap_panel.run_command("append", {"characters": "bar"})
+
+        self.window.run_command("tutkain_clear_output_view", {"output_views": ["repl"]})
+
+        self.assertEquals("", view_content(self.repl_view))
+        self.assertEquals("bar", view_content(self.tap_panel))
+
+
+        # Clear none:
+
+        reset_view(self.repl_view)
+        reset_view(self.tap_panel)
+
+        self.repl_view.run_command("append", {"characters": "foo"})
+        self.tap_panel.run_command("append", {"characters": "bar"})
+
+        self.window.run_command("tutkain_clear_output_view", {"output_views": []})
+
+        self.assertEquals("foo", view_content(self.repl_view))
+        self.assertEquals("bar", view_content(self.tap_panel))
