0322311c259fe23e66f59c8b67f92b31f37c6cf9
Eero Helenius <eero.helenius@pm.me>
2021-09-28 20:17:54 +0300

    Fix syntax definition paths

--- package.py
+++ package.py
@@ -428,7 +428,7 @@ class TutkainEvaluateCommand(TextCommand):
                     view.run_command("insert_snippet", {"contents": snippet})
 
                 view.settings().set("tutkain_repl_input_panel", True)
-                view.assign_syntax("Clojure (Tutkain).sublime-syntax")
+                view.assign_syntax("Packages/Tutkain/Clojure (Tutkain).sublime-syntax")
             elif code:
                 variables = {}
 
@@ -1133,7 +1133,7 @@ class TutkainOpenDiffWindowCommand(TextCommand):
 
         view = window.new_file()
         view.set_name("Tutkain: Diff")
-        view.assign_syntax("Clojure (Tutkain).sublime-syntax")
+        view.assign_syntax("Packages/Tutkain/Clojure (Tutkain).sublime-syntax")
         view.set_scratch(True)
         view.set_reference_document(reference)
         view.run_command("append", {"characters": actual})
