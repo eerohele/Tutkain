8cf42048b53fd131824aa3ae24500e55d6ad4e02
Eero Helenius <eero.helenius@pm.me>
2021-10-04 11:43:33 +0300

    Remove result_file_regex from REPL view
    It does nothing useful. Use Tutkain: Explore Stack Trace instead.

--- src/repl/views.py
+++ src/repl/views.py
@@ -58,7 +58,6 @@ def configure(view, dialect, host, port):
     view.settings().set("tutkain_repl_host", host)
     view.settings().set("tutkain_repl_port", port)
     view.settings().set("scroll_past_end", 0.5)
-    view.settings().set("result_file_regex", r"""\s*\[.+?\"(.+?)" (\d+)\]""")
     view.set_read_only(True)
     view.set_scratch(True)
     view.assign_syntax("REPL (Tutkain).sublime-syntax")
