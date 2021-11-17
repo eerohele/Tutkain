16b8a8c1f49e7519bbd206824006310d80ce452b
Eero Helenius <eero.helenius@pm.me>
2021-11-17 18:43:36 +0200

    Refactor local-positions for clarity

--- clojure/src/tutkain/analyzer.clj
+++ clojure/src/tutkain/analyzer.clj
@@ -79,12 +79,13 @@
   (with-open [reader (base64-reader context)]
     (let [nodes (reader->nodes file ns start-line start-column reader)
           position->unique-name (index-by-position nodes)
-          position {:form (-> form name symbol) :line line :column column :end-column end-column}]
-      (when-some [unique-name (get position->unique-name position)]
-        (eduction
-          (filter #(= unique-name (:name %)))
-          (map node->position)
-          nodes)))))
+          position {:form (-> form name symbol)
+                    :line line
+                    :column column
+                    :end-column end-column}
+          unique-name->node (group-by :name nodes)
+          unique-name (position->unique-name position)]
+      (map node->position (unique-name->node unique-name)))))
 
 (defn ^:private parse-namespace
   [ns]
