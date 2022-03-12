7acc1b9cbf2c362ec10f8f54f5f588f1aa2d9fc8
Eero Helenius <eero.helenius@pm.me>
2022-03-12 22:50:00 +0200

    Clean up comment form

--- clojure/dev/src/user.clj
+++ clojure/dev/src/user.clj
@@ -16,23 +16,8 @@
   ,,,)
 
 (comment
-  (require
-    '[clojure.repl :as repl]
-    '[tutkain.analyzer :refer [local-positions]]
-    '[tutkain.completions :refer [candidates]]
-    '[criterium.core :refer [quick-bench]])
+  (require '[tutkain.completions :refer [candidates]])
+  (require '[criterium.core :refer [quick-bench]])
 
   (quick-bench (candidates "java." 'clojure.core))
-
-  (quick-bench
-    (local-positions
-      {:file "clojure/core.clj"
-       :ns (the-ns 'clojure.core)
-       :context (with-out-str (repl/source clojure.core/doseq))
-       :form 'seq-exprs
-       :start-line 0
-       :start-column 0
-       :line 5
-       :column 4
-       :end-column 13}))
-  )
+  ,,,)
