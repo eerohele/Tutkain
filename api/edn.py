9bddd019ea30e0d80fc2c2dd1d7573b1804e55ce
Eero Helenius <31859+eerohele@users.noreply.github.com>
2025-09-22 22:12:23 +0300

    Add better attribution and license

--- api/edn.py
+++ api/edn.py
@@ -3,9 +3,17 @@
 This implementation is incomplete. There's just enough so that the Python
 plugin host can communicate with the Clojure server.
 
-The tricky parts are ported from Clojure's EdnReader.java.
-
 For more information on EDN, see https://github.com/edn-format/edn."""
+
+"""Parts of the implementation are adapted from clojure.lang.EdnReader:
+
+ Copyright (c) Rich Hickey. All rights reserved.
+ The use and distribution terms for this software are covered by the
+ Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+ which can be found in the file epl-v10.html at the root of this distribution.
+ By using this software in any fashion, you are agreeing to be bound by
+ the terms of this license.
+ You must not remove this notice, or any other, from this software."""
 
 import io
 from dataclasses import dataclass
